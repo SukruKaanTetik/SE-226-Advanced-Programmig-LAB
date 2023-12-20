7f9cf92af7391653555775e01bef18c06a5fef14
Şükrü Kaan Tetik <tetiksukrukaan@gmail.com>
2023-12-20 17:13:46 +0300

    Create Second_Sctript.py

---
+++ LAB_1/Second_Sctript.py
@@ -0,0 +1,8 @@
+var1 = int(input("Enter var1: "))
+var2 = int(input("Enter var2: "))
+
+sum = var1 + var2
+diff = var1 - var2
+prod = var1 * var2
+print(f"Var1: {var1}, Var2: {var2}\n"
+      f"Sum: {sum}, Diff: {var2}, Prod: {prod}")
