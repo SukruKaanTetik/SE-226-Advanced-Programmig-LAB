9995da9fdf0608bba48b6093f721828f97b153d2
Şükrü Kaan Tetik <tetiksukrukaan@gmail.com>
2023-12-20 17:09:44 +0300

    Create First_Script.cpp

---
+++ LAB_1/First_Script.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    string name;
+    string ID;
+
+    cout<<"What is your name?"<<endl;
+    cin>>name;
+    cout<<"Hello "<<name<<"."<<endl;
+    cout<<"What is your student name?"<<endl;
+    cin>>ID;
+    cout<<"Your ID is "<<ID<<".";
+
+    return 0;
+}
